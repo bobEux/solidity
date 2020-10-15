52aab54cc815e0b9474dd2ea5a10c828771bdca5
chriseth <chris@ethereum.org>
2020-10-15 19:16:14 +0200

    Adjust existing tests.

--- test/libsolidity/semanticTests/types/mapping_enum_key_library_encoderV2.sol
+++ test/libsolidity/semanticTests/types/mapping_enum_key_library_encoderV2.sol
@@ -1,3 +1,5 @@
+pragma experimental ABIEncoderV2;
+
 enum E { A, B, C }
 library L {
     function get(mapping(E => uint8) storage table, E k) external returns (uint8) {
@@ -18,21 +20,20 @@ contract test {
 }
 // ====
 // EVMVersion: >=byzantium
-// ABIEncoderV1Only: true
 // ----
 // library: L
 // get(uint8): 0 -> 0
 // get(uint8): 0x01 -> 0
-// get(uint8): 0xa7 -> FAILURE, hex"4e487b71", 33
+// get(uint8): 0xa7 -> FAILURE
 // set(uint8,uint8): 0x01, 0xa1 ->
 // get(uint8): 0 -> 0
 // get(uint8): 0x01 -> 0xa1
-// get(uint8): 0xa7 -> FAILURE, hex"4e487b71", 33
+// get(uint8): 0xa7 -> FAILURE
 // set(uint8,uint8): 0x00, 0xef ->
 // get(uint8): 0 -> 0xef
 // get(uint8): 0x01 -> 0xa1
-// get(uint8): 0xa7 -> FAILURE, hex"4e487b71", 33
+// get(uint8): 0xa7 -> FAILURE
 // set(uint8,uint8): 0x01, 0x05 ->
 // get(uint8): 0 -> 0xef
 // get(uint8): 0x01 -> 0x05
-// get(uint8): 0xa7 -> FAILURE, hex"4e487b71", 33
+// get(uint8): 0xa7 -> FAILURE
